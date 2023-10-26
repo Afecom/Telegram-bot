853dd4d1ec508a1dd11f655916c5604a99590134
sala kute <sala kute@DESKTOP-U93620R>
2023-10-26 14:56:23 +0300

    modification1

---
+++ webview/src/db/db.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+
+async function getData() {
+  const consumer_key = "ck_0a1d0af37fc58d63e8925d487fa92f3c17e93726";
+  const consumer_secret = "cs_54f05f6e23f158b1abde94968fb3a2d40aeb7ba7";
+  try {
+    const response = await fetch(
+      "https://aveluxecosmetics.com/wp-json/wc/v3/products?consumer_key="+consumer_key+"&consumer_secret="+consumer_secret
+    );
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch products from the API.");
+    }
+
+    const productsData = await response.json();
+
+    return productsData.map((product) => ({
+      title: product.name,
+      price: product.sale_price,
+      // Image: product.image,
+      id: product.id,
+    }));
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+// Usage example
+getData()
+  .then((products) => {
+    console.log(products);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
