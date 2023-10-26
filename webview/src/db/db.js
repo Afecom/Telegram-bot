7b85f733dd66fdeaadec5c85eabbcd69e041d04f
sala kute <sala kute@DESKTOP-U93620R>
2023-10-26 17:29:20 +0300

    test8

--- webview/src/db/db.js
+++ webview/src/db/db.js
@@ -1,6 +1,3 @@
-const consumerKey = 'ck_0a1d0af37fc58d63e8925d487fa92f3c17e93726';
-const consumerSecret = 'cs_54f05f6e23f158b1abde94968fb3a2d40aeb7ba7';
-
 // Then you can use them in your HTTP request code
 export async function getData() {
   try {
