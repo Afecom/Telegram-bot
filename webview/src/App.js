abd34aec7e12fa240e0ef593980429d181eff045
sala-kute <121769020+sala-kute@users.noreply.github.com>
2023-10-27 20:50:19 +0300

    Update App.js 8

--- webview/src/App.js
+++ webview/src/App.js
@@ -2,8 +2,8 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
-const { getResult } = require("./db/db");
-const foods = getResult();
+const { getData } = require("./db/db");
+const foods = getData();
 
 const tele = window.Telegram.WebApp;
 
