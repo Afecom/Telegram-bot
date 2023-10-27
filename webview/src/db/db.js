dfb7c3ef39d309a37349debc92bcf9657a16b22b
sala-kute <121769020+sala-kute@users.noreply.github.com>
2023-10-27 22:04:22 +0300

    Update db.js

--- webview/src/db/db.js
+++ webview/src/db/db.js
@@ -1,50 +1,40 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
 const consumerKey = 'ck_0a1d0af37fc58d63e8925d487fa92f3c17e93726';
 const consumerSecret = 'cs_54f05f6e23f158b1abde94968fb3a2d40aeb7ba7';
 
-async function getData() {
-  try {
-    const url = `https://aveluxecosmetics.com/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
-    const response = await fetch(url);
 
-    if (!response.ok) {
-      throw new Error('Request failed');
-    }
+function getData() {
+  return fetch(
+    `https://aveluxecosmetics.com/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Failed to fetch products from the API.");
+      }
 
-    const data = await response.json();
-
-    // Transform the received data into an array of product objects
-    const products = data.map((item) => {
-      return {
-        title: item.title,
-        price: item.sale_price,
-        Image: item.images[0].src,
-        id: item.id
-      };
+      return response.json();
+    })
+    .then((productsData) =>
+      productsData.map((product) => ({
+        title: product.name,
+        price: product.price,
+        Image: product.image,
+        id: product.id,
+      }))
+    )
+    .catch((error) => {
+      console.error(error);
+      return [];
     });
-
-    return products; // Return the products
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw error;
-  }
 }
 
-let result = null; // Declare the result variable
+// Usage example
+getData()
+  .then((products) => {
+    console.log(products);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
-async function fetchData() {
-  try {
-    result = await getData();
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
-
-fetchData();
-
-function getResult() {
-  return result; // Return the result from the function
-}
-
-export { getResult };
