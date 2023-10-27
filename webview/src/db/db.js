f80add73a71d275c262e044ce8ca32692201be52
sala-kute <121769020+sala-kute@users.noreply.github.com>
2023-10-27 15:50:43 +0300

    Update db.js 6

--- webview/src/db/db.js
+++ webview/src/db/db.js
@@ -3,6 +3,8 @@ import fetch from 'node-fetch';
 const consumerKey = 'your_consumer_key';
 const consumerSecret = 'your_consumer_secret';
 
+let result; // Declare the result variable outside the function
+
 async function getData() {
   try {
     const response = await fetch('https://aveluxecosmetics.com/wp-json/wc/v3/products', {
@@ -16,24 +18,26 @@ async function getData() {
     }
 
     const data = await response.json();
-    console.log('Data:', data); // Log the products to the console
-    
+
     // Transform the received data into an array of product objects
     const products = data.map((item) => {
       return {
         title: item.title,
         price: item.price,
-        Image: item.images[0].src,
+        Image: item.Image,
         id: item.id
       };
     });
 
-
-    return products;
+    result = products; // Assign the result to the variable
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
   }
 }
 
-export { getData };
+function getResult() {
+  return result; // Return the result from the function
+}
+
+export { getResult };
