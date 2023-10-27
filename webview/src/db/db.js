e96e92f310a631338595daa0da4da9cc51f4ba18
sala-kute <121769020+sala-kute@users.noreply.github.com>
2023-10-27 16:16:25 +0300

    Update db.js
    I think final

--- webview/src/db/db.js
+++ webview/src/db/db.js
@@ -3,15 +3,10 @@ import fetch from 'node-fetch';
 const consumerKey = 'ck_0a1d0af37fc58d63e8925d487fa92f3c17e93726';
 const consumerSecret = 'cs_54f05f6e23f158b1abde94968fb3a2d40aeb7ba7';
 
-let result; // Declare the result variable outside the function
-
 async function getData() {
   try {
-    const response = await fetch('https://aveluxecosmetics.com/wp-json/wc/v3/products', {
-      headers: {
-        Authorization: `Bearer ${consumerKey}:${consumerSecret}`
-      }
-    });
+    const url = `https://aveluxecosmetics.com/wp-json/wc/v3/products?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`;
+    const response = await fetch(url);
 
     if (!response.ok) {
       throw new Error('Request failed');
@@ -29,12 +24,24 @@ async function getData() {
       };
     });
 
-    result = products; // Assign the result to the variable
+    return products; // Return the products
   } catch (error) {
     console.error('Error fetching data:', error);
     throw error;
   }
 }
+
+let result = null; // Declare the result variable
+
+async function fetchData() {
+  try {
+    result = await getData();
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+fetchData();
 
 function getResult() {
   return result; // Return the result from the function
